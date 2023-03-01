e58757fe954d8184cc39d9120155c86d8ec0ed55
Roman Cleary <romancleary0@gmail.com>
2023-03-01 14:16:28 -0800

    updated  LED lights

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -37,4 +37,8 @@ void loop() {
   delay(500);                       // wait for a half second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(250);                       // wait for a quarter second
+   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);                       // wait for a half second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1500);                       // wait for a quarter second
   }
