8f166eb10058d3beda04dbdfc0d12a1f7b37c865
Roman Cleary <romancleary0@gmail.com>
2023-03-16 14:13:50 -0700

    add recouses

--- BlindSpotMonitor/BlindSpotMonitor.ino
+++ BlindSpotMonitor/BlindSpotMonitor.ino
@@ -1,6 +1,7 @@
 //Programer:Roman Cleary
 //Date: 3.16.23
 //Program:Blind Spot Monitor
+//resoure:https://projecthub.arduino.cc/Krepak/a6ea3ad5-fd23-49c9-a96c-5e6fcf97f2cc?ref=search&ref_id=red%20green%20yellow%20light%20echo%20sensor&offset=1
 void setup() {
   // put your setup code here, to run once:
 
