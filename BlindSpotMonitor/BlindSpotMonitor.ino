3c98818645a2fb982129c961e15ffc502ac0317a
Roman Cleary <romancleary0@gmail.com>
2023-03-16 14:08:52 -0700

    initial Blind spot Monitor commit

---
+++ BlindSpotMonitor/BlindSpotMonitor.ino
@@ -0,0 +1,12 @@
+//Programer:Roman Cleary
+//Date: 3.16.23
+//Program:Blind Spot Monitor
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
