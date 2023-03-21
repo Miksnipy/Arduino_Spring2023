a1479076a2e9b2f99678e59a52215373068de2b8
Roman Cleary <romancleary0@gmail.com>
2023-03-21 14:02:14 -0700

    copy & pasted code from re

--- BlindSpotMonitor/BlindSpotMonitor.ino
+++ BlindSpotMonitor/BlindSpotMonitor.ino
@@ -2,12 +2,65 @@
 //Date: 3.16.23
 //Program:Blind Spot Monitor
 //resoure:https://projecthub.arduino.cc/Krepak/a6ea3ad5-fd23-49c9-a96c-5e6fcf97f2cc?ref=search&ref_id=red%20green%20yellow%20light%20echo%20sensor&offset=1
-void setup() {
-  // put your setup code here, to run once:
 
+#define trigPin 2
+#define echoPin 3
+#define LEDlampRed 4
+#define LEDlampYellow 5
+#define LEDlampGreen 6
+#define soundbuzzer 7
+int sound = 500;
+
+
+void setup() {
+Serial.begin (9600);
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+pinMode(LEDlampRed, OUTPUT);
+pinMode(LEDlampYellow, OUTPUT);
+pinMode(LEDlampGreen, OUTPUT);
+pinMode(soundbuzzer, OUTPUT);
+}
+void loop() {
+long durationindigit, distanceincm;
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+durationindigit = pulseIn(echoPin, HIGH);
+distanceincm = (durationindigit/5) / 29.1;
+
+if (distanceincm < 50) {
+digitalWrite(LEDlampGreen, HIGH);
+}
+else {
+digitalWrite(LEDlampGreen, LOW);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+if (distanceincm < 20) {
+digitalWrite(LEDlampYellow, HIGH);
+}
+else {
+digitalWrite(LEDlampYellow,LOW);
+}
+if (distanceincm < 5) {
+digitalWrite(LEDlampRed, HIGH);
+sound = 1000;
+}
+else {
+digitalWrite(LEDlampRed,LOW);
+}
 
+if (distanceincm > 5 || distanceincm <= 0){
+Serial.println("Outside the permissible range of distances");
+noTone(soundbuzzer);
+}
+else {
+Serial.print(distanceincm);
+Serial.println(" cm");
+tone(soundbuzzer, sound);
+}
+
+delay(300);
 }
