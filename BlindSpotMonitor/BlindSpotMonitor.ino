9e5d0602a38a51e94808bc6db8e0499c34868591
Roman Cleary <romancleary0@gmail.com>
2023-03-22 13:59:03 -0700

    updated varibles

--- BlindSpotMonitor/BlindSpotMonitor.ino
+++ BlindSpotMonitor/BlindSpotMonitor.ino
@@ -1,66 +1,65 @@
-//Programer:Roman Cleary
-//Date: 3.16.23
-//Program:Blind Spot Monitor
-//resoure:https://projecthub.arduino.cc/Krepak/a6ea3ad5-fd23-49c9-a96c-5e6fcf97f2cc?ref=search&ref_id=red%20green%20yellow%20light%20echo%20sensor&offset=1
 
-#define trigPin 2
-#define echoPin 3
-#define LEDlampRed 4
-#define LEDlampYellow 5
-#define LEDlampGreen 6
-#define soundbuzzer 7
-int sound = 500;
+//progrmer: Roman Cleary
+//Date: 3.22.23
+//program: blind spot monitor
+
+int trigPin = 7;
+int echoPin = 6;
+int LEDlampBlue =11;
+int LEDlampWhite = 9;
+int LEDlampRed = 10; 
+int soundbuzzer = 3;
+int sound  = 500;
 
 
-void setup() {
-Serial.begin (9600);
-pinMode(trigPin, OUTPUT);
-pinMode(echoPin, INPUT);
-pinMode(LEDlampRed, OUTPUT);
-pinMode(LEDlampYellow, OUTPUT);
-pinMode(LEDlampGreen, OUTPUT);
-pinMode(soundbuzzer, OUTPUT);
+void setup() 
+  Serial.begin (9600);
+  pinMode(trigPin,  OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(LEDlampRed, OUTPUT);
+  pinMode(LEDlampWhite,  OUTPUT);
+  pinMode(LEDlampBlue, OUTPUT);
+  pinMode(soundbuzzer, OUTPUT);
 }
-void loop() {
-long durationindigit, distanceincm;
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-durationindigit = pulseIn(echoPin, HIGH);
-distanceincm = (durationindigit/5) / 29.1;
-
-if (distanceincm < 50) {
-digitalWrite(LEDlampGreen, HIGH);
+void  loop() 
+  long durationindigit, distanceincm;
+  digitalWrite(trigPin, LOW);  
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  durationindigit = pulseIn(echoPin, HIGH);
+  distanceincm = (durationindigit/5) / 29.1;
+ 
+  if (distanceincm < 50) 
+      digitalWrite(LEDlampBlue, HIGH);
 }
-else {
-digitalWrite(LEDlampGreen, LOW);
+  else       digitalWrite(LEDlampBlue,  LOW);
+  }
+  
+  if (distance < 20) {
+    digitalWrite(LEDlampWhite,  HIGH);
 }
-
-if (distanceincm < 20) {
-digitalWrite(LEDlampYellow, HIGH);
+  else {
+    digitalWrite(LEDlampWhite,LOW);
+  }
+  if (distance  < 5) {
+   digitalWrite(LEDlampRed, HIGH);
+    sound = 1000;
 }
-else {
-digitalWrite(LEDlampYellow,LOW);
-}
-if (distanceincm < 5) {
-digitalWrite(LEDlampRed, HIGH);
-sound = 1000;
-}
-else {
-digitalWrite(LEDlampRed,LOW);
-}
-
-if (distanceincm > 5 || distanceincm <= 0){
-Serial.println("Outside the permissible range of distances");
-noTone(soundbuzzer);
-}
-else {
-Serial.print(distanceincm);
-Serial.println(" cm");
-tone(soundbuzzer, sound);
-}
-
-delay(300);
+  else  {
+    digitalWrite(LEDlampRed,LOW);
+  }
+ 
+  if (distanceincm > 5 ||  distanceinsm <= 0){
+    Serial.println("Outside the permissible range of distances");
+    noTone(soundbuzzer);
+  }
+  else {
+    Serial.print(distance);
+    Serial.println("  cm");
+    tone(buzzer, sound);
+  }
+  
+  delay(300);
 }
